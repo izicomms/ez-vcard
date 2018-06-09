f78bdd6f6c4c3aa4f83de35902515a290cd4f698
Norman A <norman@atashbar.com>
2018-06-09 12:15:30 +1000

    Allow creating of types with any name

--- src/main/java/ezvcard/parameter/AddressType.java
+++ src/main/java/ezvcard/parameter/AddressType.java
@@ -68,7 +68,7 @@ public class AddressType extends VCardParameter {
 	@SupportedVersions({ V2_1, V3_0 })
 	public static final AddressType PREF = new AddressType("pref");
 
-	private AddressType(String value) {
+	public AddressType(String value) {
 		super(value);
 	}
 
--- src/main/java/ezvcard/parameter/TelephoneType.java
+++ src/main/java/ezvcard/parameter/TelephoneType.java
@@ -89,7 +89,7 @@ public class TelephoneType extends VCardParameter {
 
 	public static final TelephoneType WORK = new TelephoneType("work");
 
-	private TelephoneType(String value) {
+	public TelephoneType(String value) {
 		super(value);
 	}
 
